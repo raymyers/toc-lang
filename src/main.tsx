0626007fbf7e699e8754daeacdad46b94b66a669
Ray Myers <rmyers@indeed.com>
2023-01-27 10:27:49 -0600

    Add React Router

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,24 @@
 import React from "react"
+import { BrowserRouter } from "react-router-dom"
+import { createBrowserRouter, RouterProvider } from "react-router-dom"
 import ReactDOM from "react-dom/client"
 import App from "./App"
 import "./index.css"
+import ErrorPage from "./ErrorPage"
+
+const router = createBrowserRouter(
+  [
+    {
+      path: "/",
+      element: <App />,
+      errorElement: <ErrorPage />
+    }
+  ],
+  { basename: "/toc-lang" }
+)
 
 ReactDOM.createRoot(document.getElementById("root-reset")).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 )
